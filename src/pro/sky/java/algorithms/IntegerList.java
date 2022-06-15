46428e06b3b265bd90509a3797bcb3d7d00e6bb0
vivatgreen <grindsasha@bk.ru>
2022-06-15 22:15:09 +0300

    Домашнее задание 2.15

--- src/pro/sky/java/algorithms/IntegerList.java
+++ src/pro/sky/java/algorithms/IntegerList.java
@@ -1,11 +1,9 @@
 package pro.sky.java.algorithms;
-
-public interface StringList {
-
+public interface IntegerList {
     // Добавление элемента.
     // Вернуть добавленный элемент
     // в качестве результата выполнения.
-    String add(String item);
+    int add(int item);
 
     // Добавление элемента
     // на определенную позицию списка.
@@ -14,7 +12,7 @@ public interface StringList {
     // выбросить исключение.
     // Вернуть добавленный элемент
     // в качестве результата выполнения.
-    String add(int index, String item);
+    int add(int index, int item);
 
     // Установить элемент
     // на определенную позицию,
@@ -23,44 +21,44 @@ public interface StringList {
     // если индекс меньше
     // фактического количества элементов
     // или выходит за пределы массива.
-    String set(int index, String item);
+    int set(int index, int item);
 
     // Удаление элемента.
     // Вернуть удаленный элемент
     // или исключение, если подобный
     // элемент отсутствует в списке.
-    String remove(String item);
+    int removeByValue(int item);
 
-    // Удаление элемента по индексу.
-    // Вернуть удаленный элемент
-    // или исключение, если подобный
-    // элемент отсутствует в списке.
-    String remove(int index);
+    //     Удаление элемента по индексу.
+//     Вернуть удаленный элемент
+//     или исключение, если подобный
+//     элемент отсутствует в списке.
+    int remove(int index);
 
     // Проверка на существование элемента.
     // Вернуть true/false;
-    boolean contains(String item);
+    boolean contains(int item);
 
     // Поиск элемента.
     // Вернуть индекс элемента
     // или -1 в случае отсутствия.
-    int indexOf(String item);
+    int indexOf(int item);
 
     // Поиск элемента с конца.
     // Вернуть индекс элемента
     // или -1 в случае отсутствия.
-    int lastIndexOf(String item);
+    int lastIndexOf(int item);
 
     // Получить элемент по индексу.
     // Вернуть элемент или исключение,
     // если выходит за рамки фактического
     // количества элементов.
-    String get(int index);
+    int get(int index);
 
     // Сравнить текущий список с другим.
     // Вернуть true/false или исключение,
     // если передан null.
-    boolean equals(StringList otherList);
+    boolean equals(IntegerList otherList);
 
     // Вернуть фактическое количество элементов.
     int size();
@@ -76,5 +74,8 @@ public interface StringList {
     // Создать новый массив
     // из строк в списке
     // и вернуть его.
-    String[] toArray();
+    int[] toArray();
+
+    //Переработанный метод contains
+    boolean containsBinary (int item);
 }
--- src/pro/sky/java/algorithms/NullPointerException.java
+++
@@ -1,4 +0,0 @@
-package pro.sky.java.algorithms;
-
-public class NullPointerException extends RuntimeException {
-}
